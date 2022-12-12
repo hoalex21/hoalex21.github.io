45332b54dcb0fd372902bab1050cb24fd3f94512
Alex Ho <ho.alex100@gmail.com>
2022-12-12 11:07:19 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
+// React
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as ReactDOM from 'react-dom';
+
+// React Router
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+// React Components
+import {
+  Index,
+  About,
+} from './main.js';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />
+  },
+  {
+    path: "/about",
+    element: <About />
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// export is required for other files to use these components
+
+
+export function Index() {
+    return (
+      <h1>Index</h1>
+    );
+}
+
+export function About() {
+    return (
+      <h1>About</h1>
+    );
+}
