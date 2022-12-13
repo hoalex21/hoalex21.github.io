8577b8ebe3c0abd336299da87183e4d011793bd5
Alex Ho <ho.alex100@gmail.com>
2022-12-12 21:27:05 -0500

    first commit

--- src/skills.js
+++ src/skills.js
@@ -6,6 +6,8 @@ export function SkillsText() {
     return (
         <div className='container'>
             <h1 className='display-5'>Skills</h1>
+            {/* Programming Languages: Python Java JavaScript SQL C# HTML CSS */}
+            {/* FrameWorks: Flask, React */}
         </div>
     );
 }
