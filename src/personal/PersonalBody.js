04818a35cc5bc53761e9299ddd9a3d583d0f2b5e
Alex Ho <ho.alex100@gmail.com>
2023-05-23 20:58:59 -0400

    remove old calorie tracker link

--- src/personal/PersonalBody.js
+++ src/personal/PersonalBody.js
@@ -10,7 +10,6 @@ export default function PersonalBody() {
             <h1 className='display-5'>Personal Projects</h1>
             <ul>
                 <li><Link to="caltrak">CalTrak</Link> (May 2023)</li>
-                <li><a href="https://expo.dev/@aleton/CalorieTracker" target="_blank" rel="noreferrer">Calorie Tracker</a> (December 2022)</li>
                 <li><Link to="veryberry">Very Berry</Link> (June 2020)</li>
             </ul>
         </div>
