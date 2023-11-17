d42fd8c18536f56eb5d60175edb605d971064468
Alex Ho <alexho@Alexs-Air.fios-router.home>
2023-11-16 21:05:03 -0500

    add background poster

--- src/intro/IntroBackground.js
+++ src/intro/IntroBackground.js
@@ -3,11 +3,19 @@ import React from 'react';
 
 // Media
 import background from '../media/background.mp4';
+import backgroundPoster from '../media/background-poster.jpg';
 
 
 export default function IntroBackground() {
     return (
-        <video muted loop autoPlay playsInline className='w-100 vh-100 d-block object-fit-cover position-absolute z-index-1 mask-image-topdown-opacity'>
+        <video 
+            muted 
+            loop 
+            autoPlay 
+            playsInline 
+            className='w-100 vh-100 d-block object-fit-cover position-absolute z-index-1 mask-image-topdown-opacity'
+            poster={backgroundPoster}
+        >
             <source src={background} type='video/mp4' />
         </video>
     );
