ff0dc9c225040b6260bca4c97060404cb8dc8e0c
Alex Ho <alexho@Alexs-Air.fios-router.home>
2023-11-18 22:53:48 -0500

    test click on blubby dodge link

---
+++ selenium/src/test/java/personal/links/Links.java
@@ -0,0 +1,6 @@
+package personal.links;
+
+public class Links {
+    public String localUrl = "http://localhost:3000/";
+    public String productionUrl = "https://hoalex21.github.io";
+}
--- selenium/src/test/java/personal/personal/PersonalTest.java
+++ selenium/src/test/java/personal/personal/PersonalTest.java
@@ -2,17 +2,34 @@ package personal.personal;
 
 import org.junit.jupiter.api.Test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.JavascriptExecutor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import personal.links.Links;
 
 public class PersonalTest {
+    Links links = new Links();
     WebDriver driver = new ChromeDriver();
 
     @Test
-    public void testOpenCloseChrome() {
+    public void testBlubbyDodgeLink() {
         try {
-            driver.get("localhost:3000");
+            driver.get(links.localUrl);
+
+            WebElement blubbyDodgeLink = driver.findElement(By.linkText("Blubby Dodge"));
+
+            JavascriptExecutor executor = (JavascriptExecutor) driver;
+            executor.executeScript("arguments[0].click()", blubbyDodgeLink);
+
+            // Clicking did not work. Even when adding explicit and implicit waits.
+            // blubbydodgeLink.click();
+
+            assertTrue(driver.getCurrentUrl().endsWith("#/blubbydodge"));
         } finally {
             driver.close();
         }
