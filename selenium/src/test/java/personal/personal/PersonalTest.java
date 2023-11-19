a57398031e15f297fdfd0ca059d19d91b6797bcd
Alex Ho <alexho@Alexs-Air.fios-router.home>
2023-11-18 23:55:06 -0500

    add test for going back from project pages

--- selenium/src/test/java/personal/personal/PersonalTest.java
+++ selenium/src/test/java/personal/personal/PersonalTest.java
@@ -2,9 +2,11 @@ package personal.personal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,6 +19,10 @@ public class PersonalTest {
     Links links = new Links();
     WebDriver driver = new ChromeDriver();
 
+    String backLinkText = "< Back";
+
+    String testLinkUrl = links.localUrl;
+
     @ParameterizedTest
     @CsvSource({
             "Blubby Dodge, #/blubbydodge",
@@ -28,7 +34,7 @@ public class PersonalTest {
             String expectedPath
     ) {
         try {
-            driver.get(links.localUrl);
+            driver.get(testLinkUrl);
 
             WebElement link = driver.findElement(By.linkText(linkText));
 
@@ -38,7 +44,40 @@ public class PersonalTest {
             // Clicking did not work. Even when adding explicit and implicit waits.
             // link.click();
 
+            WebElement backButton = driver.findElement(By.linkText(backLinkText));
+
             assertTrue(driver.getCurrentUrl().endsWith(expectedPath));
+            assertTrue(backButton.isDisplayed());
+            assertTrue(driver.findElements(By.linkText(linkText)).isEmpty());
+        } finally {
+            driver.close();
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Blubby Dodge",
+            "CaloTrak",
+            "Very Berry"
+    })
+    public void testPersonalProjectPagesGoBackToIndex(String linkText) {
+        try {
+            driver.get(testLinkUrl);
+
+            WebElement link = driver.findElement(By.linkText(linkText));
+
+            JavascriptExecutor executor = (JavascriptExecutor) driver;
+            executor.executeScript("arguments[0].click()", link);
+
+            // Clicking did not work. Even when adding explicit and implicit waits.
+            // link.click();
+
+            WebElement backButton = driver.findElement(By.linkText(backLinkText));
+            backButton.click();
+
+            assertTrue(driver.getCurrentUrl().endsWith(testLinkUrl));
+            assertTrue(driver.findElement(By.linkText(linkText)).isDisplayed());
+            assertTrue(driver.findElements(By.linkText(backLinkText)).isEmpty());
         } finally {
             driver.close();
         }
