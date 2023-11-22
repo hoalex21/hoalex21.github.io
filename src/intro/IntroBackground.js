e472cc75fb8a31cd132db05d3e27f4940c4a04de
Alex Ho <alexho@alexs-air.mynetworksettings.com>
2023-11-22 01:11:42 -0500

    use js to autoplay video

--- src/intro/IntroBackground.js
+++ src/intro/IntroBackground.js
@@ -3,20 +3,21 @@ import React from 'react';
 
 // Media
 import background from '../media/background.mp4';
-import backgroundPoster from '../media/background-poster.jpg';
 
 
 export default function IntroBackground() {
     return (
-        <video 
-            muted 
-            loop 
-            autoPlay 
-            playsInline 
-            className='w-100 vh-100 d-block object-fit-cover position-absolute z-index-1 mask-image-topdown-opacity'
-            poster={backgroundPoster}
-        >
-            <source src={background} type='video/mp4' />
-        </video>
+        <>
+            <video
+                muted
+                loop
+                autoPlay
+                playsInline
+                id="background-video"
+                className='w-100 vh-100 d-block object-fit-cover position-absolute z-index-1 mask-image-topdown-opacity'
+            >
+                <source src={background} type='video/mp4' />
+            </video>
+        </>
     );
 }
