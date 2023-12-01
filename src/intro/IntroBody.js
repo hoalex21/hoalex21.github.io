b6a9a3a3ef332d9a853b205aa0211ca17af25bd7
Alex Ho <alexho@Alexs-Air.fios-router.home>
2023-12-01 16:15:04 -0500

    highlight intro text

--- src/intro/IntroBody.js
+++ src/intro/IntroBody.js
@@ -5,9 +5,10 @@ import React from 'react';
 export default function IntroBody() {
     return (
         <div className='container'>
-            <div className='d-flex vh-100 text-center align-items-center'>
-                <div className='w-100'>
+            <div className='d-flex vh-100 text-center align-items-center justify-content-center'>
+                <div className='bg-light rounded p-3 align-self-center'>
                     <h1 className='display-1 d-inline' id='name'>Alex Ho</h1>
+                    <br />
                     <h2 className='display-6'>Portfolio</h2>
                 </div>
             </div>
