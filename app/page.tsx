50771e13539a8eaa52deb5c9ef90606421d65227
Alex Ho <ho.alex100@gmail.com>
2024-03-24 23:16:57 -0400

    implement about me page only

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
-import IntroBackground from '@/components/intro/IntroBackground'
-import IntroBody from '@/components/intro/IntroBody'
-import AboutBody from '@/components/about/AboutBody'
-import ExperienceBody from '@/components/experience/ExperienceBody'
-import PersonalBody from '@/components/personal/PersonalBody'
-import SocialBody from '@/components/social/SocialBody'
+import About from '@/components/root/About'
 
 export default function Home() {
   return (
-    <>
-        <IntroBackground />
-        <IntroBody />
-        <AboutBody />
-        <ExperienceBody />
-        <PersonalBody />
-        <SocialBody />
-    </>
+    <div className='container mx-auto mt-10'>
+        <About />
+    </div>
   );
 }
---
+++ components/root/About.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function About() {
+    return (
+        <div>
+            <div className="flex justify-center">
+                <img src="/1655484593486.jpg" className="rounded-full" />
+            </div>
+
+            <p className="text-5xl">About Me</p>
+
+            <br></br>
+
+            <div className='container text-center mx-auto'>
+                <p className="text-xl">
+                    I'm Alex and I'm currently studying for my B.S. in Computer Science at Drexel University with an anticipated graduation in June 2024.
+                    I have a passion for technology and expressing myself creatively through software.
+                    Whether it be a great web browsing experience or a quirky game, I hope there is something that I can provide you through my work. &hearts;
+                </p>
+            </div>
+        </div>
+    );
+}
