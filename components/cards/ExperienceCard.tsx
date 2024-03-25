3e82e3ceb7eb4e866ed345c05c6fd89287d4d6ec
Alex Ho <ho.alex100@gmail.com>
2024-03-25 11:32:39 -0400

    add project cards

--- components/cards/ExperienceCard.tsx
+++ components/cards/ExperienceCard.tsx
@@ -2,7 +2,8 @@ interface Props {
     title: string;
     img: string;
     alt: string;
-    bullets: string[];
+    bullets?: string[];
+    description?: string;
     time: string;
 }
 
@@ -10,14 +11,28 @@ export default function ExperienceCard(props: Props) {
     return (
         <div className='rounded-md border-solid border-2 border-white relative'>
             <div className='flex justify-center bg-white p-8'>
-                <img src={props.img} alt={props.alt} className="max-h-20" />
+                <img src={props.img} alt={props.alt} className="max-h-36" />
             </div>
 
             <div className="px-4 py-2 mb-10">
                 <p className='text-2xl m-2'>{props.title}</p>
-                <ul className="list-disc mx-4">
-                    {props.bullets.map(bullet => <li className="mb-2">{bullet}</li>)}
-                </ul>
+                {
+                    props.bullets?
+                    <ul className="list-disc mx-4">
+                        {props.bullets.map(bullet => <li className="mb-2">{bullet}</li>)}
+                    </ul>
+                    :
+                    null
+                }
+
+                {
+                    props.description?
+                    <p className="mx-4 mb-2">
+                        {props.description}
+                    </p>
+                    :
+                    null
+                }
             </div>
 
             <div className="bg-white text-black py-2 px-4 absolute bottom-0 w-full">
--- components/home/Projects.tsx
+++ components/home/Projects.tsx
@@ -1,7 +1,37 @@
+import ExperienceCard from "../cards/ExperienceCard";
+
 export default function Projects() {
     return (
         <div>
             <p className="text-5xl">Personal Projects</p>
+
+            <br />
+
+            <div className="grid grid-cols-3 gap-10">
+                <ExperienceCard
+                    title="Blubby Dodge"
+                    img="blubbydodgeicon.png"
+                    alt="Blubby Dodge Icon"
+                    description="Your reflexes and quick thinking will be put to the ultimate test! Challenge yourself to beat your high score with each run. The objective is simple yet exhilarating: dodge and weave through a barrage of pillars raining down from above. As Blubby dashes along, players will immerse themselves in a colorful, cartoon-inspired setting with a touch of whimsy."
+                    time="Published: August 31, 2023"
+                />
+
+                <ExperienceCard
+                    title="CaloTrak"
+                    img="calotrak1024x500.png"
+                    alt="CaloTrak Logo"
+                    description="A simple, easy-to-use lightweight calorie tracking app. Calories in, calories out. No macro or micronutrient information is provided. Useful for quickly logging your meals and snacks without needing the details. Calories do not reset after each day. No registration is required. The data is saved to your local device."
+                    time="Published: May 30, 2023"
+                />
+
+                <ExperienceCard
+                    title="Very Berry"
+                    img="VeryBerry.png"
+                    alt="Very Berry player sprite"
+                    description="A class project involving a group of 4. This is a space-shooter bullet hell type game where you operate a strawberry vessel and survive an onslaught of endless blueberry drones. The boss - evil emoji guy - will appear if you survive long enough."
+                    time="Published: June 202"
+                />
+            </div>
         </div>
     );
 }
